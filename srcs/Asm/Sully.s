80ecfd68cf705f5bf79cfe570b7ca2311c6f7161
avan-pra <velovo.vanpraet@gmail.com>
2022-03-08 18:40:52 +0000

    enhanced sully, need to check stack pointer

--- srcs/Asm/Sully.s
+++ srcs/Asm/Sully.s
@@ -1,7 +1,26 @@
-SECTION .text
 global main
+extern sprintf
+extern printf
 extern exit
 
+SECTION .data
+BUFFER times 100 db 0
+FILE db "test.c",0
+
+SECTION .text
 main:
-	mov rdi, 32
-	call exit
+	push rbp		; save a copy of rbp
+	mov rbp, rsp	; 
+
+	mov rdi, BUFFER
+	mov rsi, FILE
+	mov al, 0
+	call sprintf
+
+	mov rdi, BUFFER
+	mov al, 0
+	call printf
+
+	mov rsp, rbp
+	pop rbp
+	ret
